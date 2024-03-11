f0cd5de1bd1df8266acfe1fa6630fa48a82cd018
Murat GOKDUMAN <drmuratgokduman@gmail.com>
2024-03-11 21:04:43 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [hotels, setHotels] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch("/.netlify/functions/getHotels");
+    const responseBody = await response.json();
+    setHotels(responseBody.data.hotel_data.values);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(hotels);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Hotels</h1>
+      {hotels?.map((hotel) => (
+        <div className="hotel" key={hotel.id}>
+          <h2>{hotel.name}</h2>
+          <p>{hotel.rating}</p>
+        </div>
+      ))}
     </div>
   );
 }
